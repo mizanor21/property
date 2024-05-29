2118089065cd30c351db1a68534246869e790a97
mizan21 <mizanorrahman398@gmail.com>
2024-05-30 00:20:33 +0600

    Nav Contact part updated

--- src/components/Shared/Navbar/NavContact.jsx
+++ src/components/Shared/Navbar/NavContact.jsx
@@ -1,26 +1,49 @@
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 import { MdWifiCalling3 } from "react-icons/md";
+// eslint-disable-next-line no-unused-vars
 import { TbWorldCheck } from "react-icons/tb";
 import { TfiEmail } from "react-icons/tfi";
 
 const NavContact = () => {
   return (
-    <div className="bg-green-700 text-white font-mono lg:flex gap-5 justify-center hidden">
-      <span className="flex items-center gap-2">
-        <MdWifiCalling3 />
-        +88 01925 507 436(BD)
-      </span>
-      <span className="flex items-center gap-2">
-        <MdWifiCalling3 />
-        +971525709908(UAE)
-      </span>
-      <span className="flex items-center gap-2">
-        <TfiEmail />
-        jpnusvc@gmail.com
-      </span>
-      <span className="flex items-center gap-2">
-        <TbWorldCheck />
-        www.jpnbd.com
-      </span>
+    <div className="bg-green-700">
+      <div className=" text-white lg:flex justify-between hidden container mx-auto">
+        <div className=" flex gap-3 items-center">
+          <FaFacebook />
+          <FaTwitter />
+          <FaYoutube />
+        </div>
+        <div className="flex gap-5">
+          <span className="flex items-center gap-2">
+            <MdWifiCalling3 />
+            <a href="tel:+88 01925 507 436" className="link-hover">
+              +88 01925 507 436
+            </a>
+            <span>(BD)</span>
+          </span>
+          <span className="flex items-center gap-2">
+            <MdWifiCalling3 />
+            <a href="tel:+971525709908" className="link-hover">
+              +971525709908
+            </a>
+            <span>(UAE)</span>
+          </span>
+          <span className="flex items-center gap-2 link-hover">
+            <TfiEmail />
+            <a href="mailto: jpnusvc@gmail.com">jpnusvc@gmail.com</a>
+          </span>
+          {/* <span className="flex items-center gap-2 link-hover">
+          <TbWorldCheck />
+          <a
+            href="http://www.jpnbd.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            www.jpnbd.com
+          </a>
+        </span> */}
+        </div>
+      </div>
     </div>
   );
 };

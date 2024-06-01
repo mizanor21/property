19b536bf8d576d78e53149bb2cc27ee30714c0e7
mizan21 <mizanorrahman398@gmail.com>
2024-06-01 09:50:08 +0600

    Services part added

--- src/components/Home/Home/HomePage.jsx
+++ src/components/Home/Home/HomePage.jsx
@@ -2,12 +2,14 @@ import Agents from "../Agents/Agents";
 import Banner from "../Banner/Banner";
 import InterestContact from "../InterestContact/InterestContact";
 import LatestProperty from "../LatestProperty/LatestProperty";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <LatestProperty></LatestProperty>
+      <Services></Services>
       <Agents></Agents>
       <InterestContact></InterestContact>
     </div>
---
+++ src/components/Home/Services/Services.jsx
@@ -0,0 +1,106 @@
+import img1 from "../../../assets/images/services/1.png";
+import img2 from "../../../assets/images/services/2.jpeg";
+import img3 from "../../../assets/images/services/3.png";
+import img4 from "../../../assets/images/services/4.png";
+import img5 from "../../../assets/images/services/5.png";
+import img6 from "../../../assets/images/services/6.png";
+import img7 from "../../../assets/images/services/7.png";
+import img8 from "../../../assets/images/services/8.png";
+const Services = () => {
+  return (
+    <div className="bg-slate-50 py-10">
+      <div className="container mx-auto">
+        <div>
+          <h2 className="text-3xl text-balance border-l-4 pl-5 border-green-600">
+            Our Services
+          </h2>
+        </div>
+        <div className="my-5 flex flex-wrap-reverse gap-5">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img1} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img1} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img2} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img3} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img4} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img5} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img6} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img7} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="flex justify-center items-center mt-5">
+              <img src={img8} alt="Shoes" className="rounded-xl w-24" />
+            </div>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">RECRUITMENT</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
